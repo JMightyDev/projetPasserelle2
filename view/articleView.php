bbc7d544f7ef37692455608c7d0f1cffc4d8a7ce
JMighty <jeanmichel.sonn@gmail.com>
2024-09-01 00:13:41 +0200

    encore !

--- view/articleView.php
+++ view/articleView.php
@@ -20,8 +20,8 @@
                 </div>
                 <?php if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == 1) { ?>
                     <div class="card-footer text-end">
-                        <a class="btn btn-warning me-3" href="<?= $path ?>/<?= $article['id'] ?>/edit"><i class="bi bi-pencil"></i> Modifier</a>
-                        <a class="btn btn-danger" href="<?= $path ?>/<?= $article['id'] ?>/delete"><i class="bi bi-x-lg"></i> Supprimer</a>
+                        <a class="btn btn-warning me-3" href="<?= $path ?><?= $article['id'] ?>/edit"><i class="bi bi-pencil"></i> Modifier</a>
+                        <a class="btn btn-danger" href="<?= $path ?><?= $article['id'] ?>/delete"><i class="bi bi-x-lg"></i> Supprimer</a>
                     </div>
                 <?php } ?>
             </div>
@@ -30,7 +30,7 @@
         <?php
             if (isset($_SESSION['email'])) {
         ?>
-            <form class="mb-4" method="post" action="<?= $path ?>/<?= $article['id'] ?>/addComment">
+            <form class="mb-4" method="post" action="<?= $path ?><?= $article['id'] ?>/addComment">
                 <div class="mb-3 col-lg-6">
                     <label for="content" class="form-label">Commentaire</label>
                     <textarea class="form-control" id="content" rows="2" name="content"></textarea>
