9cc427460fc122058bf001822625a8494bb4ee75
JMighty <jeanmichel.sonn@gmail.com>
2024-09-01 10:11:21 +0200

    pareil

--- index.php
+++ index.php
@@ -33,8 +33,7 @@ function url(){
     return sprintf(
       "%s://%s%s",
       isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-      $_SERVER['SERVER_NAME'],
-      $_SERVER['REQUEST_URI']
+      $_SERVER['SERVER_NAME']
     );
 }
 
