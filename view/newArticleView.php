e91ef0ee3d21d80a7f950704c5dc8ac120faf8f9
JMighty <jeanmichel.sonn@gmail.com>
2024-09-01 09:35:47 +0200

    Correction path dans page de modif d'articles

--- view/newArticleView.php
+++ view/newArticleView.php
@@ -11,10 +11,10 @@
         ?>
         <?php if (isset($editMode)) { ?>
             <h1 class="text-dark my-4">Modifier le projet</h1>
-            <form method="post" action="<?= $path ?>article/<?= $article['id'] ?>/edit">
+            <form method="post" action="<?= $article['id'] ?>/edit">
         <?php } else { ?>
             <h1 class="text-dark my-4">Ajouter le projet</h1>
-            <form method="post" action="<?= $path ?>newArticle">
+            <form method="post" action="newArticle">
         <?php } ?>
             <div class="mb-3">
                 <label for="title" class="form-label">Titre de l'article</label>
